65f2c354ca09d334b81dc70aaf323f54963d6fdc
owengitdev <yanyida19790228@gmail.com>
2026-09-09 23:19:57 -0400

    HTML

---
+++ script.js
@@ -0,0 +1,10 @@
+<><h1 class="mb-1" id="header1">Software Development</h1>
+
+element.addEventListener("click", showAlert);
+
+function showAlert() 
+    alert("Hello, Looks like you're now ready to dive into Software Development!");
+{"}"}
+
+
+<p id="header2">Welcome to the Software Development Website! We are going to learn some prerequisites for software development and education before getting a job in the field.</p></>
